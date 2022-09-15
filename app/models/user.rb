246c7c7f59099625547a0dedfd42b51e378b5e0f
Namiho1994 <namiho.matano@gmail.com>
2022-09-15 09:42:56 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+  validates :nickname, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Input full-width characters"}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Input full-width characters"}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Input full-width katakana characters"}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Input full-width katakana characters"}
+  validates :birth_date, presence: true
+
 end
