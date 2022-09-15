d5ff73f41d9df25162077e09aea9411f4586864c
Namiho1994 <namiho.matano@gmail.com>
2022-09-16 07:27:38 +0900

    バリデーションの設定

---
+++ app/models/item.rb
@@ -0,0 +1,31 @@
+class Item < ApplicationRecord
+
+
+  #belongs_to :user
+  #has_one :order
+  has_one_attached :image
+
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :item_scheduled_delivery
+
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, presence: true
+  validates :sales_status_id, presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :item_scheduled_delivery_id, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_orequal_to: 9_999_999 }
+  validates :user_id, presence: true
+
+
+  validates :category_id, numericality: { other_than: 0 } 
+  validates :sales_status_id, numericality: { other_than: 0 } 
+  validates :shipping_fee_status_id, numericality: { other_than: 0 } 
+  validates :prefecture_id, numericality: { other_than: 0 } 
+  validates :item_scheduled_delivery_id, numericality: { other_than: 0 } 
+  
+end
