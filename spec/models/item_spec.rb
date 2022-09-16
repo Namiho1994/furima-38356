c8778ee198f2137ca49385af652b3e312153eedc
Namiho1994 <namiho.matano@gmail.com>
2022-09-16 12:45:02 +0900

    テストコードの記載

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,98 @@
-require 'rails_helper'
-
+require 'rails_helper'  
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.create(:item)
+    sleep(1)
+  end
+
+  describe '商品の出品登録' do
+    context '出品登録ができるとき' do
+      it '全ての入力事項が、存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品ができないとき' do
+      it '商品画像がないと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名がないと出品できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品の説明がないと出品できない' do
+        @item.info = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+      it 'カテゴリーの情報がないと出品できない' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'カテゴリーの情報が「---」だと出品できない' do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category must be other than 0')
+      end
+      it '商品の状態の情報がないと出品できない' do
+        @item.sales_status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status can't be blank")
+      end
+      it '商品の状態の情報が「---」だと出品できない' do
+        @item.sales_status_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Sales status must be other than 0')
+      end
+      it '配送料の負担の情報がないと出品できない' do
+        @item.shipping_fee_status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
+      end
+      it '配送料の負担の情報が「---」だと出品できない' do
+        @item.shipping_fee_status_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping fee status must be other than 0')
+      end
+      it '発送元の地域の情報がないと出品できない' do
+        @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送元の地域の情報が「---」だと出品できない' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 0')
+      end
+      it '発送までの日数の情報がないと出品できない' do
+        @item.item_scheduled_delivery_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item scheduled delivery can't be blank")
+      end
+      it '発送までの日数の情報が「---」だと出品できない' do
+        @item.item_scheduled_delivery_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item scheduled delivery must be other than 0')
+      end
+      it '価格の情報がないと出品できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number')
+      end
+      it '価格は、¥300~¥9,999,999の間でないと出品できない' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it '価格は半角数値でないと保存できない' do
+        @item.price = '１００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end    
 end
