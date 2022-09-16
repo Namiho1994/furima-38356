67ad66014c367ab26b7ebcaaa60017854ec7e326
Namiho1994 <namiho.matano@gmail.com>
2022-09-16 14:51:33 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,7 +18,7 @@ class Item < ApplicationRecord
   validates :shipping_fee_status_id, presence: true
   validates :prefecture_id, presence: true
   validates :item_scheduled_delivery_id, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_orequal_to: 9_999_999 },
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_orequal_to: 9_999_999 },
                     format: { with: /\A[0-9]+\z/ }
   validates :user_id, presence: true
   validates :image, presence: true
